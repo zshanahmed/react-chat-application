825555083e3020c01a82909d855d711217878389
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-10-08 14:21:23 -0500

    Added login form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
 
 const App = () => {
+
+  if (!localStorage.getItem('username')) return <LoginForm />
   return (
     <ChatEngine
       height="100vh"
       projectID="223b04a6-cd55-41e1-b0d7-aea99a4057b2"
-      userName="zeeshan"
-      userSecret="gugly123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
